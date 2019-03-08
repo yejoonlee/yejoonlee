d912a7c033ba8dd07c82ff9d1125d570e0bb2dfc
이예준 <yejoonlee617@gmail.com>
2019-03-08 16:36:11 +0000

    ignore_test

--- past/w2v_wv/functions_for_w2v.py
+++
@@ -1,8 +0,0 @@
-from nltk import regexp_tokenize
-
-def tokenize(contents):
-    pattern = "[\w']+"
-    tokenized_contents = regexp_tokenize(contents, pattern)
-    # filtered_sentence = [w for w in tokenized_contents if not w in stop_words]
-    return tokenized_contents
-
